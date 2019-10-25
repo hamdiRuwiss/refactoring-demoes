ca0063794eca6d9831bc85be4462b463656eb004
Hebatulrahman <56227082+Hebatulrahman@users.noreply.github.com>
2019-10-26 00:46:08 +0300

    updating 1

--- RefactoringSamples/MethodLevel/ConditionalComplexity/GildedRoseConsoleProgram.cs
+++ RefactoringSamples/MethodLevel/ConditionalComplexity/GildedRoseConsoleProgram.cs
@@ -10,13 +10,13 @@ namespace RefactoringSamples.MethodLevel.ConditionalComplexity
         IList<Item> Items;
         static void Main2(string[] args)
         {
-            System.Console.WriteLine("OMGHAI!");
+            System.Console.WriteLine("Hi THERE!");
 
             var app = new GildedRoseConsoleProgram()
             {
                 Items = new List<Item>
                                           {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                                              new Item {Name = "+5 Dexterity Vest", SellIn = 12, Quality = 20},
                                               new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                                               new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                                               new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
